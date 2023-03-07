a41ab4cf82d5b19796b6e00cbf8d2f872b1fe7d2
SariNeverfinal <sari@neverfinal.com>
2023-03-07 11:17:28 +0100

    add set up

---
+++ frontend/src/components/ImagePicker/ImagePicker.js
@@ -0,0 +1,12 @@
+import { useRef, useEffect }  from 'react'; 
+
+function ImagePicker () {
+  
+    return (
+        <div>
+            <input type="file"/>
+        </div>
+    )
+}
+
+export default ImagePicker;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import ImagePicker from "../components/ImagePicker/ImagePicker";
+
+function Home() {
+  return (
+    <div className="p-10 space-y-6">
+        <h1 className="font-bold text-2xl">Image Picker from Video</h1>
+        <ImagePicker/>
+    </div>
+  );
+}
+
+export default Home;
