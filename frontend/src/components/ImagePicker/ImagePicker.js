48355e83492f27e133d73dae3aad43e419a6eea9
SariNeverfinal <sari@neverfinal.com>
2023-03-07 11:33:51 +0100

    add videp upload

--- frontend/src/components/ImagePicker/ImagePicker.js
+++ frontend/src/components/ImagePicker/ImagePicker.js
@@ -1,10 +1,24 @@
-import { useRef, useEffect }  from 'react'; 
+import { useState, useEffect }  from 'react'; 
 
 function ImagePicker () {
   
+    const [videoPath, setVideoPath] = useState();
+
+    const handleVideoUpload = (event) => {
+        setVideoPath(URL.createObjectURL(event.target.files[0]));
+    };
+
+   //URL.createObjectURL(event.target.files[0])
+
     return (
-        <div>
-            <input type="file"/>
+        <div className='space-y-4'>
+            <input type="file" onChange={ handleVideoUpload }/>
+            {videoPath ? 
+                <video controls muted width="400">
+                    <source src={videoPath} type="video/mp4"></source>
+                </video>
+                : <></>
+            }
         </div>
     )
 }
