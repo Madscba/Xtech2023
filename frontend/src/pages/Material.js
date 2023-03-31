23ac7e0a66a7e5d26eb8fff04d0969d8eaf70082
SariNeverfinal <sari@neverfinal.com>
2023-03-31 14:10:44 +0200

    add basic materials page

--- frontend/src/pages/Material.js
+++ frontend/src/pages/Material.js
@@ -1,4 +1,5 @@
 import Wrapper from "../layouts/Wrapper";
+import NumberLabel from "../components/base/Labels/NumberLabel";
 
 function Material() {
     return (
@@ -7,7 +8,20 @@ function Material() {
                 <a href="/dashboard">
                     <p className="text-indigo-500"><strong>Go back to dashboard</strong></p>
                 </a>
-                <h2>How to?</h2>
+                <div className="space-y-6 w-2/3">
+                    <div className="space-y-2">
+                        <h2>How to?</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing diam donec adipiscing tristique risus nec feugiat. Cursus euismod quis viverra nibh cras pulvinar mattis nunc sed</p>
+                    </div>
+                    <ul className="space-y-6">
+                        <li className="number-bullet-point"><NumberLabel>1</NumberLabel>Viverra mauris in aliquam sem fringilla ut morbi tincidunt. </li>
+                        <li className="number-bullet-point"><NumberLabel>2</NumberLabel>Viverra mauris in aliquam sem fringilla ut morbi tincidunt. </li>
+                        <li className="number-bullet-point"><NumberLabel>3</NumberLabel>Viverra mauris in aliquam sem fringilla ut morbi tincidunt. </li>
+                        <li className="number-bullet-point"><NumberLabel>4</NumberLabel>Viverra mauris in aliquam sem fringilla ut morbi tincidunt. </li>
+                        <li className="number-bullet-point"><NumberLabel>5</NumberLabel>Viverra mauris in aliquam sem fringilla ut morbi tincidunt. </li>
+                        <li className="number-bullet-point"><NumberLabel>6</NumberLabel>Viverra mauris in aliquam sem fringilla ut morbi tincidunt. </li>
+                    </ul>
+                </div>
             </div>
         </Wrapper>
     );
