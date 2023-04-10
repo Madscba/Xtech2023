2369873b2d85477e7d7e48bc1cff06ed548ce80b
SariNeverfinal <sari@neverfinal.com>
2023-04-10 02:39:00 +0200

    update color

--- frontend/src/components/base/Navigation/TopNav.js
+++ frontend/src/components/base/Navigation/TopNav.js
@@ -1,6 +1,6 @@
 function TopNav() {
     return (
-        <div className="flex flex-row justify-end w-full transparent p-5 bg-indigo-500">
+        <div className="flex flex-row justify-end w-full transparent p-5 bg-teal-500">
             <div className="bg-white w-8 h-8 rounded-full"></div>
         </div>
     );
--- frontend/src/pages/NoPage.js
+++ frontend/src/pages/NoPage.js
@@ -1,7 +1,7 @@
 function NoPage() {
   return (
     <div className="bg-gray-200/50 h-screen w-screen flex items-center justify-center">
-       <h1 className="text-indigo-500 pb-4">No Page Found</h1>
+       <h1 className="text-teal-500 pb-4">No Page Found</h1>
     </div>
   );
 }
