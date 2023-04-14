5eafc42afe154d0d74151a423b1cd01c7a290b82
SariNeverfinal <sari@neverfinal.com>
2023-04-14 02:39:33 +0200

    add logo

--- frontend/src/components/base/Navigation/TopNav.js
+++ frontend/src/components/base/Navigation/TopNav.js
@@ -1,7 +1,13 @@
 function TopNav() {
     return (
-        <div className="flex flex-row justify-end w-full transparent p-5 bg-teal-500">
-            <div className="bg-white w-8 h-8 rounded-full"></div>
+        <div className="flex flex-row justify-between w-full transparent py-5 px-10 bg-teal-500">
+            <a href="/dashboard">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                    <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+                </svg>
+            </a>
+            <div className="text-white">Mia</div>
         </div>
     );
   }
