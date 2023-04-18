40c0609a25bd9694ac320254c559ab63d39c0c19
SariNeverfinal <sari@neverfinal.com>
2023-04-18 08:48:34 +0200

    hide secret person

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -5,7 +5,7 @@ import { useState }  from 'react';
 
 function Dashboard() {
 
-    const [personWasAdded, setPersonWasAdded] = useState(true);
+    const [personWasAdded, setPersonWasAdded] = useState(false);
 
     const feedbacks = [
         {
