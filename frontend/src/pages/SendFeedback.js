86b08947dc424b05e520d27fc9cb578652e670af
SariNeverfinal <sari@neverfinal.com>
2023-05-17 13:12:42 +0200

    fix loading on sending screen

--- frontend/src/pages/SendFeedback.js
+++ frontend/src/pages/SendFeedback.js
@@ -81,7 +81,9 @@ function SendFeedback() {
             const date = new Date(jsonData.data.submission.created_at);
             setSubmissionDate(date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear());
             setLoadingFeedback(false);
+            console.log("done")
         } catch (error) {
+            console.log(error)
             handleError();
         }
     }
@@ -101,54 +103,52 @@ function SendFeedback() {
 
     return (
         <>
-            { loadingFeedback && <Loading/>}
-            { !loadingFeedback && <Wrapper>
-                <div className="container">
-                    <BackButton/>
-                        {loadingFeedback && <Loading/>}
-                        {(!loadingFeedback && error ) && <p className="error-msg">{error}</p>}
-                        {(!loadingFeedback && !error ) &&
-                            <>
-                                <div className="space-y-8">
-                                    <div className="flex flex-col gap-2 items-start">
-                                        <h2>Make a referral for {feedback.submission.patient.first_name}'s submission</h2>
-                                        <p>Case #{feedback?.submission?.id}</p>
-                                        {submissionDate && <p>Date: {submissionDate}</p>}
-                                    </div>
-                                    <div className="space-y-2">
-                                        <h3>Choose a doctor</h3>
-                                        <select className="px-4 py-2 rounded-md" onChange={(e) => handleChange(e)}>
-                                            {doctors.map((doctor, index) => (
-                                                <option key={index} value={doctor.first_name}>Dr. {doctor.first_name} {doctor.last_name}</option>
-                                            ))}
-                                        </select>
-                                    </div>
-                                    <div className="space-y-2">
-                                        <h3>What text will be sent</h3>
-                                        <p className="max-w-text-limit"> Dear {selectedDoctor},<br/> 
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu.<br/>
-                                            Kind regards, <br/>
-                                            Mie
-                                        </p>
-                                    </div>
-                                    <div className="space-y-4">
-                                        <h3>What images will be attached</h3>
-                                        <div className="w-full flex flex-col sm:flex-row gap-4">
-                                            {feedback?.submitted_eyes?.length > 0 && feedback.submitted_eyes.map((eye, index) => (
-                                                <div className="flex flex-col gap-4 w-full sm:w-1/3">
-                                                    <span className="font-bold capitalize">{eye.eye_side} eye</span>
-                                                    <img className="w-full h-auto object-contain object-center" src={`${process.env.REACT_APP_API_URL}${eye.image}`}/>
-                                                </div>
-                                            ))}
+         <Wrapper>
+            <div className="container">
+                <BackButton/>
+                {loadingFeedback && <Loading/>}
+                {(!loadingFeedback && error ) && <p className="error-msg">{error}</p>}
+                {(!loadingFeedback && feedback && !error ) &&
+                    <>
+                        <div className="space-y-8">
+                            <div className="flex flex-col gap-2 items-start">
+                                <h2>Make a referral for {feedback.submission.patient.first_name}'s submission</h2>
+                                <p>Case #{feedback?.submission?.id}</p>
+                                {submissionDate && <p>Date: {submissionDate}</p>}
+                            </div>
+                            <div className="space-y-2">
+                                <h3>Choose a doctor</h3>
+                                <select className="px-4 py-2 rounded-md" onChange={(e) => handleChange(e)}>
+                                    {doctors.map((doctor, index) => (
+                                        <option key={index} value={doctor.first_name}>Dr. {doctor.first_name} {doctor.last_name}</option>
+                                    ))}
+                                </select>
+                            </div>
+                            <div className="space-y-2">
+                                <h3>What text will be sent</h3>
+                                <p className="max-w-text-limit"> Dear {selectedDoctor},<br/> 
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu.<br/>
+                                    Kind regards, <br/>
+                                    Mie
+                                </p>
+                            </div>
+                            <div className="space-y-4">
+                                <h3>What images will be attached</h3>
+                                <div className="w-full flex flex-col sm:flex-row gap-4">
+                                    {feedback?.submitted_eyes?.length > 0 && feedback.submitted_eyes.map((eye, index) => (
+                                        <div className="flex flex-col gap-4 w-full sm:w-1/3">
+                                            <span className="font-bold capitalize">{eye.eye_side} eye</span>
+                                            <img className="w-full h-auto object-contain object-center" src={`${process.env.REACT_APP_API_URL}${eye.image}`}/>
                                         </div>
-                                    </div>
+                                    ))}
                                 </div>
-                            <button className="button" onClick={sendReferral}>Make the referral</button>
-                        </>
-                        }
-                </div>
-            </Wrapper>
-            }
+                            </div>
+                        </div>
+                        <button className="button" onClick={sendReferral}>Make the referral</button>
+                    </>
+                }
+            </div>
+        </Wrapper>
         </>
     );
   }
